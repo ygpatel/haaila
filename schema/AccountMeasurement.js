23d868b0cf6a00305059648d2966959488f0c341
yogular <ygpatel@Yogeshs-Air.hsd1.ma.comcast.net>
2017-02-17 14:23:01 -0500

    Added measurement functionality

--- schema/AccountMeasurement.js
+++ schema/AccountMeasurement.js
@@ -4,9 +4,13 @@ exports = module.exports = function(app, mongoose) {
   var accountMeasurementSchema = new mongoose.Schema({
     account_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' },
     profile_name: { type: String, default: ''},
-    measurements : [mongoose.modelSchemas.Measurement],
+    measurement_id : {type: String, ref: 'Measurement' },
+    measurement_data :  [{
+        code : String,
+        value: String
+    }]
   });
-  accountMeasurementSchema.index({'account_id': 1, 'Measurement._id': 1, 'profile_name':1}, {unique: true});
+  accountMeasurementSchema.index({'account_id': 1, 'Measurement_id': 1, 'profile_name':1}, {unique: true});
   accountMeasurementSchema.set('autoIndex', (app.get('env') === 'development'));
   app.db.model('AccountMeasurement', accountMeasurementSchema, 'accountmeasurements');  
 };
